0b2ca54381ed57ffe98a287aee99d95b30c6741a
Emile Kott <emilekott@gmail.com>
2012-11-23 12:27:51 +0000

    added vimeo button

--- functions.php
+++ functions.php
@@ -1,5 +1,32 @@
 <?php
 include 'vimeo.php';
+
+function register_button( $buttons ) {
+   array_push( $buttons, "|", "vimeo" );
+   return $buttons;
+}
+
+function add_plugin( $plugin_array ) {
+   $plugin_array['vimeo'] = get_template_directory_uri() . '/js/vimeo.js';
+   return $plugin_array;
+}
+
+function my_vimeo_button() {
+
+   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') ) {
+      return;
+   }
+
+   if ( get_user_option('rich_editing') == 'true' ) {
+      add_filter( 'mce_external_plugins', 'add_plugin' );
+      add_filter( 'mce_buttons', 'register_button' );
+   }
+
+}
+
+add_action('init', 'my_vimeo_button');
+
+
 // Load utility functions
 require_once (TEMPLATEPATH . '/admin/utilities.php');
   
